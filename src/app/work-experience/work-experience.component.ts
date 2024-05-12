ec2555a4a49235664a5daa1eff2cd1bdaeaa5b31
AnthonyGTH <antogua_41@hotmail.com>
2024-05-12 01:12:30 -0600

    feat/AG-03: Improve presentation details and more.

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UtilitiesService } from '../tools/utilities.service';
 
 @Component({
-  selector: 'app-work-experience',
+  selector: 'app-work-experience-detailed',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.scss'],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  version: require('../../package.json').version,
+  angularCli: require('../../package.json').devDependencies['@angular/cli'],
+  typescript: require('../../package.json').devDependencies['typescript'],
 };
